86130a2bf1b8428a6ecd0876053d12180118ee7c
Dasturchi04 <153491609+Matnazar-Matnazarov@users.noreply.github.com>
2024-01-27 23:57:31 +0500

    Update uquv_markaz.sql

--- mysql_python/uquv_markaz.sql
+++ mysql_python/uquv_markaz.sql
@@ -1,17 +1,3 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               10.3.22-MariaDB - mariadb.org binary distribution
--- Операционная система:         Win64
--- HeidiSQL Версия:              11.0.0.5958
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 -- Дамп структуры базы данных uquv_markaz
 DROP DATABASE IF EXISTS `uquv_markaz`;
 CREATE DATABASE IF NOT EXISTS `uquv_markaz` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
